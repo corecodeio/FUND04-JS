0857f4a2529b28f2adb124c193b51e69f5570fbb
Yosef <yosefmarr@gmail.com>
2023-01-03 20:02:02 -0600

    JS: Standup 00

---
+++ W06/00stdp.js
@@ -0,0 +1,28 @@
+/*
+
+Este codigo da error, por el modo strict
+
+'use strict';
+x = 90;
+console.log(x);
+
+*/
+
+// lossly equality (value equality), strict equality (type equality)
+
+// lossly equality (==)
+// strict equality (===)
+
+let a = true;
+let b = false;
+let c = 100;
+let d = 0;
+let e = -4;
+
+// falsy and thruthly values
+// console.log(a == c); // false
+
+console.log(b == '');
+console.log(b === '');
+console.log(10 == '10');
+console.log(10 === '10');
